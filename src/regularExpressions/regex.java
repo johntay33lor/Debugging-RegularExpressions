0f5ce31205e09cfa4bab0caec3b2fc0f2daa5964
John <johntay33lor@gmail.com>
2023-05-23 19:38:58 -0400

    Initial commit

--- src/regularExpressions/regex.java
+++ src/regularExpressions/regex.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class regex {
     public static void main(String[] args) {
-        // Validating email address
+        // Validating email address:
         String emailRegex = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b";
         String email = "johntaylor@gmail.com";
         String email2 = "johntaylor.gmail.com";
@@ -13,7 +13,7 @@ public class regex {
         System.out.println("Is " + email + " valid? " + isEmailValid);
         System.out.println("Is " +  email2 + " valid? " + isEmail2Valid);
 
-        // Validating phone number
+        // Validating phone number:
         String phoneNumberRegex = "\\(\\d{3}\\) \\d{3}-\\d{4}";
         String phoneNumber = "(123) 456-7890";
         String phoneNumber2 = "1234567890";
@@ -22,7 +22,7 @@ public class regex {
         System.out.println("Is " + phoneNumber + " valid? " + isPhoneNumberValid);
         System.out.println("Is " + phoneNumber2 + " valid? " + isPhoneNumber2Valid);
 
-        // Validating date
+        // Validating date:
         String dateRegex = "^(0[1-9]|1[0-2])/(0[1-9]|[12]\\d|3[01])/(19|20)\\d{2}$";
         String date = "05/18/1996";
         String date2 = "05-18-1996";
@@ -31,7 +31,7 @@ public class regex {
         System.out.println("Is " + date + " valid? " + isDateValid);
         System.out.println("Is " + date2 + " valid? " + isDate2Valid);
 
-        // Validating URL
+        // Validating URL:
         String urlRegex = "^(http|https)://([a-zA-Z0-9]+\\.)+[a-zA-Z]{2,}$";
         String url = "https://www.example.com";
         String url2 = "example.com";
@@ -40,7 +40,7 @@ public class regex {
         System.out.println("Is " + url + " valid? " + isUrlValid);
         System.out.println("Is " + url2 + " valid? " + isUrl2Valid);
 
-        // Validating credit card number
+        // Validating credit card number:
         String creditCardRegex = "\\b\\d{4}[- ]?\\d{4}[- ]?\\d{4}[- ]?\\d{4}\\b";
         String creditCardNumber = "1234-5678-9012-3456";
         String creditCardNumber2 = "1234/5678/9012/3456";
@@ -49,7 +49,7 @@ public class regex {
         System.out.println("Is " + creditCardNumber + " valid? " + isCreditCardValid);
         System.out.println(("Is " + creditCardNumber2 + " valid? " + isCreditCard2Valid));
 
-        // Validating US zip code
+        // Validating US zip code:
         String zipCodeRegex = "^\\d{5}(-\\d{4})?$";
         String zipCode = "28273";
         String zipCode2 = "28273-00";
@@ -58,7 +58,7 @@ public class regex {
         System.out.println("Is " + zipCode + " valid? " + isZipCodeValid);
         System.out.println("Is " + zipCode2 + " valid? " + isZipCode2Valid);
 
-        // Validating Java identifier
+        // Validating Java identifier:
         String javaIdentifierRegex = "^[a-zA-Z_$][a-zA-Z\\d_$]*$";
         String javaIdentifier = "myVariable";
         String javaIdentifier2 = "3myVariable";
@@ -67,7 +67,7 @@ public class regex {
         System.out.println("Is " +  javaIdentifier + " valid? " + isJavaIdentifierValid);
         System.out.println("Is " + javaIdentifier2 + " valid? " + isJavaIdentifier2Valid);
 
-        // Validating hexadecimal color code
+        // Validating hexadecimal color code:
         String colorCodeRegex = "^#([0-9a-fA-F]{6})$";
         String colorCode = "#FFC0CB";
         String colorCode2 = "FFC0CB#";
@@ -76,7 +76,7 @@ public class regex {
         System.out.println("Is " + colorCode + " valid? " + isColorCodeValid);
         System.out.println("Is " + colorCode2 + " valid " + isColorCode2Valid);
 
-        // Validating time in 24-hour format
+        // Validating time in 24-hour format:
         String timeRegex = "^([01]\\d|2[0-3]):[0-5]\\d$";
         String time = "12:34";
         String time2 = "25:00";
